516b8774a311c389839f58a0d2f33acb250f7248
hming <huming@youjiajk.com>
2022-12-23 13:24:06 +0800

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import { ref } from 'vue'
+
+const isCollapse = ref(true)
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+</script>
+
+<template>
+  <div class="container">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">
+          <el-menu
+            class="el-menu-vertical-demo"
+            :collapse="isCollapse"
+            @open="handleOpen"
+            @close="handleClose"
+          >
+            <el-sub-menu index="1">
+              <template #title>
+                <span>Navigator One</span>
+              </template>
+              <el-menu-item-group>
+                <template #title><span>Group One</span></template>
+                <el-menu-item index="1-1">item one</el-menu-item>
+                <el-menu-item index="1-2">item two</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="Group Two">
+                <el-menu-item index="1-3">item three</el-menu-item>
+              </el-menu-item-group>
+              <el-sub-menu index="1-4">
+                <template #title><span>item four</span></template>
+                <el-menu-item index="1-4-1">item one</el-menu-item>
+              </el-sub-menu>
+            </el-sub-menu>
+            <el-menu-item index="2">
+              <template #title >
+                <span>Navigator Two</span>
+              </template>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <template #title>Navigator Three</template>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <template #title>Navigator Four</template>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+        <el-main>
+          <RouterView />
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+}
+.el-menu-vertical-demo {
+  width: 100%;
+  height: 100%;
+}
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+.el-menu--collapse>.el-menu-item-group>ul>.el-sub-menu>.el-sub-menu__title>span, .el-menu--collapse>.el-menu-item>span, .el-menu--collapse>.el-sub-menu>.el-sub-menu__title>span {
+  width: 100%;
+  height: 100%;
+  visibility: initial;
+}
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container">
+    index page 
+  </div>
+</template>
+
+<style scoped>
+
+</style>
